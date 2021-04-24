5f2e22beee069f2d466af84e71389ea212f8df23
KATAR7 <74004757+KATAR7@users.noreply.github.com>
2021-04-24 20:15:37 +0500

    v0.5.1 source

---
+++ unit4.pas
@@ -0,0 +1,102 @@
+unit Unit4;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Buttons, StdCtrls;
+
+type
+
+  { TForm4 }
+
+  TForm4 = class(TForm)
+    BitBtn1: TBitBtn;
+    Button1: TButton;
+    Button2: TButton;
+    ComboBox1: TComboBox;
+    Label1: TLabel;
+    tbcheck: TCheckBox;
+    diln: TCheckBox;
+    procedure BitBtn1Click(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure ComboBox1Change(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure tbcheckChange(Sender: TObject);
+    procedure dilnChange(Sender: TObject);
+    procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
+    procedure FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+  private
+
+  public
+
+  end;
+
+var
+  Form4: TForm4;
+
+implementation
+Uses Unit1;
+
+{$R *.lfm}
+
+{ TForm4 }
+
+procedure TForm4.Button1Click(Sender: TObject);
+begin
+  Form1.ColorDialog.Execute;
+end;
+
+procedure TForm4.Button2Click(Sender: TObject);
+begin
+  Form4.Close;
+end;
+
+procedure TForm4.ComboBox1Change(Sender: TObject);
+begin
+  Case ComboBox1.Caption of
+       'Auto both': Form1.Editor.ScrollBars:=ssAutoBoth;
+       'Auto horizontal': Form1.Editor.ScrollBars:=ssAutoHorizontal;
+       'Auto vertical': Form1.Editor.ScrollBars:=ssAutoVertical;
+       'Both': Form1.Editor.ScrollBars:=ssBoth;
+       'Horizontal': Form1.Editor.ScrollBars:=ssHorizontal;
+       'None': Form1.Editor.ScrollBars:=ssNone;
+       'Vertical': Form1.Editor.ScrollBars:=ssVertical;
+  end;
+end;
+
+procedure TForm4.FormShow(Sender: TObject);
+begin
+  Form1.Sync();
+end;
+
+procedure TForm4.tbcheckChange(Sender: TObject);
+begin
+  Form1.ToolBar1.Visible := tbcheck.Checked;
+end;
+
+procedure TForm4.dilnChange(Sender: TObject);
+begin
+  Form1.Editor.Gutter.Visible := diln.Checked;
+end;
+
+procedure TForm4.FormClose(Sender: TObject; var CloseAction: TCloseAction);
+begin
+
+end;
+
+procedure TForm4.FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState
+  );
+begin
+  If (key=27) then close();
+end;
+
+procedure TForm4.BitBtn1Click(Sender: TObject);
+begin
+  Form1.FontDialog.Execute;
+end;
+
+end.
+
